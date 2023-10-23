910183ef74249e6029df055ddbc18264f6d3a610
mand000-0 <sopia0315@naver.com>
2023-10-23 11:06:10 +0900

    ver1 commit

---
+++ lib/button/x_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class XButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String svgAsset;
+
+  const XButton({Key? key, required this.onPressed, required this.svgAsset})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.transparent, // 버튼 색상
+        padding: const EdgeInsets.all(0), // 내부 패딩 제거
+        elevation: 0, // 그림자 제거
+      ),
+      child: SvgPicture.asset(svgAsset), // SVG 파일 로드
+    );
+  }
+}
