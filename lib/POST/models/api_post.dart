c1ae054a01bcdefe2cd4653033af2caee3bdef53
SeonHyeonWook <seonhyunuk@gmail.com>
2023-11-29 05:04:12 +0900

    http 통신 예제

--- lib/POST/models/api_post.dart
+++ lib/POST/models/api_post.dart
@@ -1,34 +1,66 @@
-// api_postboard.dart
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<String> PostBoard(String postName, String title, String mainText, String postType) async {
-  try {
-    final baseUrl = "https://kauqj.shop";
-    final endpoint = "/app/posts";
+final String baseUrl = 'https://kauqj.shop/app/posts';
 
-    final response = await http.post(
-      Uri.parse(baseUrl + endpoint),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        'postName': postName, //게시판 종류 ex) 자유, 동아리
-        'title': title, //제목
-        'mainText': mainText, //내용
-        'postType': postType, //어느 게시판인지???
-      }),
-    );
+Future<void> createPost() async {
+  final response = await http.post(
+    Uri.parse('$baseUrl/board/posts'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode({
+      'postName': 'postName',
+      'title': 'title',
+      'mainText': 'mainText',
+      'postType': 'postType',
+    }),
+  );
 
-    if (response.statusCode == 200) {
-      print("good");
-      return '성공';
-    } else {
-      return '실패';
-    }
-  } catch (e) {
-    print('PostBoard 함수에서 에러 발생: $e');
-    return '에러 발생';
+  if (response.statusCode == 200) {
+    print('게시글 생성 성공: ${response.body}');
+  } else {
+    print('게시글 생성 실패: ${response.statusCode}');
+  }
+}
+
+Future<void> getPosts() async {
+  final response = await http.get(Uri.parse('$baseUrl/board/posts'));
+
+  if (response.statusCode == 200) {
+    print('게시글 조회 성공: ${response.body}');
+  } else {
+    print('게시글 조회 실패: ${response.statusCode}');
+  }
+}
+
+Future<void> updatePost(String postId) async {
+  final response = await http.patch(
+    Uri.parse('$baseUrl/board/posts/$postId'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode({
+      'postName': 'updatedName',
+      'title': 'updatedTitle',
+      'mainText': 'updatedText',
+      'postType': 'updatedType',
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    print('게시글 수정 성공: ${response.body}');
+  } else {
+    print('게시글 수정 실패: ${response.statusCode}');
+  }
+}
+
+Future<void> deletePost(String postId) async {
+  final response = await http.delete(Uri.parse('$baseUrl/board/posts/$postId'));
+
+  if (response.statusCode == 200) {
+    print('게시글 삭제 성공: ${response.body}');
+  } else {
+    print('게시글 삭제 실패: ${response.statusCode}');
   }
 }
