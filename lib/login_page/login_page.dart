74c4bf2c48c6d45cf0b2f7b6609768714a82dadc
SeonHyeonWook <seonhyunuk@gmail.com>
2023-12-09 23:20:41 +0900

    로그인 성공 시 알림창 없앰

--- lib/login_page/login_page.dart
+++ lib/login_page/login_page.dart
@@ -23,44 +23,32 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Future<void> _login() async {
-  String userId = _emailController.text;
-  String userPw = _passwordController.text;
-  try {
-    // 이 부분에서 context를 추가해서 loginUser 함수를 호출합니다.
-    String result = await loginUser(userId, userPw, context);
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('로그인 결과'),
-        content: Text(result),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: const Text('확인'),
-          ),
-        ],
-      ),
-    );
-  } catch (error) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('로그인 실패'),
-        content: Text(error.toString()),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: const Text('확인'),
-          ),
-        ],
-      ),
-    );
+    String userId = _emailController.text;
+    String userPw = _passwordController.text;
+    try {
+      // 이 부분에서 context를 추가해서 loginUser 함수를 호출합니다.
+      String result = await loginUser(userId, userPw, context);
+      
+      // 로그인 성공 시 메시지를 디버그 콘솔에 출력합니다.
+      print('로그인 성공: $result');
+    } catch (error) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('로그인 실패'),
+          content: Text(error.toString()),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('확인'),
+            ),
+          ],
+        ),
+      );
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
